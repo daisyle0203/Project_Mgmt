4328fd335c78c87463facdac4953440f35033a16
Daisy Le <daisy.le0203@gmail.com>
2023-01-02 00:37:09 -0600

    delete client mutation using apollo cache

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,30 @@ import Header from "./components/Header"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 import Client from "./components/Client"
 
+// Create an InMemoryCache
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 // Create an Apollo Client
 const client = new ApolloClient({
   uri: "http://localhost:3001/graphql",
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
