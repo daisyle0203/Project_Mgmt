db5a00217c7bfbf2828fd4821524a633f1a73782
Daisy Le <daisy.le0203@gmail.com>
2023-01-01 18:15:05 -0600

    project type and query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,23 @@
-const { project, clients } = require("../sampleData.js")
+const { projects, clients } = require("../sampleData.js")
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require("graphql")
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql")
+
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+})
 
 // Client Type
 const ClientType = new GraphQLObjectType({
@@ -16,6 +33,28 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        // Get data from db
+        return projects
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // Get data from db
+        return projects.find((project) => project.id === args.id)
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        // Get data from db
+        return clients
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
@@ -28,5 +67,5 @@ const RootQuery = new GraphQLObjectType({
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
+  query: RootQuery,
 })
