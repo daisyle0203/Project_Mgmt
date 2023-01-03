f0b7d216903ca085205bd82b2b82e51aa4dcbcb2
Daisy Le <daisy.le0203@gmail.com>
2023-01-02 22:27:42 -0600

    fetch and display projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 import Client from "./components/Client"
+import Projects from "./components/Projects";
 import AddClientModal from "./components/AddClientModal";
 
 // Create an InMemoryCache
@@ -36,6 +37,7 @@ function App() {
         <Header />
         <div className="container">
           <AddClientModal />
+          <Projects />
           <Client />
         </div>
       </ApolloProvider>
